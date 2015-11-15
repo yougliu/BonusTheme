315623d9419b343799e43b6d627cda2182467516
yougliu <wumumawl@163.com>
2015-11-15 19:22:55 +0800

    ThemeRom add backView

--- app/src/main/java/themerom/bonus/com/themerom/activity/DownloadTheme.java
+++ app/src/main/java/themerom/bonus/com/themerom/activity/DownloadTheme.java
@@ -2,20 +2,40 @@ package themerom.bonus.com.themerom.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import themerom.bonus.com.themerom.R;
+import themerom.bonus.com.themerom.view.BackImage;
 
 /**
  * Created by bonus on 11/14/15.
  * Class name ${type_name}
  */
-public class DownloadTheme extends Activity{
+public class DownloadTheme extends Activity implements BackImage.OnBackClickListener{
     private static final String TAG = DownloadTheme.class.getSimpleName();
-
+    private BackImage backView;
+    private TextView mThemeTitle;
+    private ViewPager mViewPager;
+    private Button zambia, download, delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.download_theme_layout);
+        initView();
+    }
+
+    private void initView() {
+        backView = (BackImage) findViewById(R.id.id_back);
+        backView.setOnBackClickListener(this);
+    }
+
+    @Override
+    public void onBackClick() {
+        Log.d("bonus","------------------ onbackClick");
+        finish();
     }
 }
---
+++ app/src/main/java/themerom/bonus/com/themerom/view/BackImage.java
@@ -0,0 +1,81 @@
+package themerom.bonus.com.themerom.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import themerom.bonus.com.themerom.R;
+
+/**
+ * Created by Bonus on 2015/11/15 0015.
+ */
+public class BackImage extends View implements View.OnClickListener{
+    private Paint mPaint;
+    private int strokeWidth = 8;
+    private int mWidth;
+    private int mHeight;
+    private double widthMargin;
+    private double heightMargin;
+    private OnBackClickListener mListener;
+
+    public BackImage(Context context) {
+        this(context, null);
+    }
+
+    public BackImage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOnClickListener(this);
+    }
+
+    public BackImage(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthModel = MeasureSpec.getMode(widthMeasureSpec);
+        int heightModel = MeasureSpec.getMode(heightMeasureSpec);
+        Log.d("bonus","model = "+widthModel+", "+heightModel+", "+MeasureSpec.AT_MOST+", "+MeasureSpec.EXACTLY);
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+        widthMargin = mWidth*0.3;
+        heightMargin = mHeight*0.3;
+        Log.d("bonus","spec = "+mWidth+", "+mHeight);
+        setMeasuredDimension(mWidth,mHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(getResources().getColor(R.color.title_text_color));
+        mPaint.setStrokeWidth(strokeWidth);
+        canvas.drawLine((float)(mWidth-widthMargin),(float)heightMargin,(float)widthMargin,mHeight/2,mPaint);
+        canvas.drawLine((float)widthMargin,mHeight/2,(float)(mWidth-widthMargin),(float)(mHeight-heightMargin),mPaint);
+        canvas.save();
+        canvas.rotate(180,mWidth/2,mHeight/2);
+    }
+
+    public void setOnBackClickListener(OnBackClickListener  listener){
+        this.mListener = listener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("bonus","-------------onClick");
+        if(v != null){
+            mListener.onBackClick();
+        }else{
+            new RuntimeException("back click view is null");
+        }
+    }
+
+    public interface OnBackClickListener{
+        void onBackClick();
+    }
+}
